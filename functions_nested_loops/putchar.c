94138e7ee195da6d3adac5bf8429a2f3a65a8d62
heefCodes <abdulkareemtoyin277@gmail.com>
2022-11-05 19:24:27 +0000

    print _putchar

---
+++ functions_nested_loops/putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - prints _putchar followed by new line
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
