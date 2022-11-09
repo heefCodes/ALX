9c0b3ddf75c2dac444a7d39252473ef9f47140d8
heefCodes <abdulkareemtoyin277@gmail.com>
2022-11-09 15:05:11 +0000

    initial commit

---
+++ more_functions_nested_loops/5-more-numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints 0 to 14 in 10 places
+  *
+  * Return: nothing
+  */
+
+void more_numbers(void)
+{
+	char num;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num / 10 > 0)
+			
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
