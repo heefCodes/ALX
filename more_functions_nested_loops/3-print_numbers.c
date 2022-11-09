e8f775c358319931fb74492de1369a367a9622b7
heefCodes <abdulkareemtoyin277@gmail.com>
2022-11-09 14:34:19 +0000

    print 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - prints 0 to 9
+  *
+  * Return: nothing
+  */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
